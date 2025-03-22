bbd9228c0c9af398064704a3ab7e895691f55d5b
Tomas Prokop <tomas.prokop@thenetw.org>
2025-03-21 22:11:40 -0700

    Restructured UI tests

--- src/Dataverse/templates/pp-test-ui/.template.scripts/Cleanup.ps1
+++ src/Dataverse/templates/pp-test-ui/.template.scripts/Cleanup.ps1
@@ -1,8 +1,8 @@
-$folderPath = "pp-test-ui\scripts"
+# $folderPath = "scripts"
 
-if (Test-Path $folderPath) 
-{
-    Remove-Item -Path $folderPath -Recurse -Force
-} 
+# if (Test-Path $folderPath) 
+# {
+#     Remove-Item -Path $folderPath -Recurse -Force
+# } 
 
 Remove-Item .template.scripts -Recurse -Force
