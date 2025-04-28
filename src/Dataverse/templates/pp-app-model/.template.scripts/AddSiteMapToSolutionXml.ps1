6b51951a0325e8cfdb3378e128b83078c5abfcf3
Alexander <aleksander.zekelin@networg.com>
2025-04-28 14:24:30 +0200

    Mock names renamed

--- src/Dataverse/templates/pp-app-model/.template.scripts/AddSiteMapToSolutionXml.ps1
+++ src/Dataverse/templates/pp-app-model/.template.scripts/AddSiteMapToSolutionXml.ps1
@@ -6,8 +6,8 @@ $solutionPath = Resolve-Path -Path 'SolutionDeclarationsRoot/Other/Solution.xml'
 $rootComponents = $File.SelectSingleNode("//RootComponents")
 
 $newComponent = $File.CreateElement("RootComponent")
-$newComponent.SetAttribute("type", '60')
-$newComponent.SetAttribute("id", '{quickCreateFormId}')
+$newComponent.SetAttribute("type", '62')
+$newComponent.SetAttribute("schemaName", '$USERPREFIXEXAMPLE$_$APPNAMEEXAMPLE$')
 $newComponent.SetAttribute("behavior", '0')
 
 # Append the new component to the root components without writing output to console
@@ -15,5 +15,6 @@ $null = $rootComponents.AppendChild($newComponent)
 
 # Save the updated XML back to the file
 $File.Save($solutionPath)
+
 
 
---
+++ src/Dataverse/templates/pp-form-row/.template.scripts/Cleanup.ps1
@@ -0,0 +1,2 @@
+#Remove-Item .template.scripts -Recurse -Force
+#Remove-Item .template.temp -Recurse -Force
